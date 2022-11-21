7fa4648177e7f667f1db15036fc9762620a76616
ligen131 <1353055672@qq.com>
2022-11-22 02:52:33 +0800

    :sparkles: feat(externalSort): external sort

--- externalSort/externalsort.go
+++ externalSort/externalsort.go
@@ -5,25 +5,31 @@ import (
 	"encoding/binary"
 	"errors"
 	"fmt"
+	"io"
 	"math"
 	"os"
 	"sort"
+	"sync"
 
 	"externalsort/util"
 )
 
 const (
-	DATA_FOLDER                  = "./data/"
-	IN_FILE_SUFFIX               = ".in"
-	OUT_FILE_SUFFIX              = ".out"
-	INTERAL_SORT_TMP_FILE_SUFFIX = ".in.tmp"
-	MERGE_SORT_TMP_FILE_SUFFIX   = ".me.tmp"
-	READ_FILE_BUFFER_SIZE        = 4096
-	WRITE_FILE_BUFFER_SIZE       = 4096
+	DATA_FOLDER            = "./data/"
+	TMP_FOLDER             = "./data/tmp/"
+	IN_FILE_SUFFIX         = ".in"
+	OUT_FILE_SUFFIX        = ".out"
+	TMP_FILE_SUFFIX        = ".tmp"
+	READ_FILE_BUFFER_SIZE  = 4096
+	WRITE_FILE_BUFFER_SIZE = 4096
 )
 
 type ExternalSort struct {
-	blockNum int
+	filename     string
+	blockNum     int
+	blockSize    []uint64
+	tmpCount     int
+	tmpCountLock sync.Mutex
 }
 
 func (s *ExternalSort) writeToBuffer(fout *bufio.Writer, x uint64) (err error) {
@@ -37,53 +43,173 @@ func (s *ExternalSort) writeToBuffer(fout *bufio.Writer, x uint64) (err error) {
 	return nil
 }
 
-func (s *ExternalSort) chanWriteToBuffer(fout *bufio.Writer, c <-chan uint64) (err error) {
-	for x := range c {
-		err = s.writeToBuffer(fout, x)
-		if err != nil {
-			return err
-		}
+func (s *ExternalSort) writeToFile(fout *os.File, x uint64) (err error) {
+	tmp := make([]byte, 8)
+	binary.LittleEndian.PutUint64(tmp, x)
+	_, err = fout.Write(tmp)
+	if err != nil {
+		util.ErrorPrint(err, nil, "Write to file failed.")
+		return err
 	}
 	return nil
 }
 
-func (s *ExternalSort) readByteToUint64(fin *bufio.Reader,
-	size uint64, c chan<- uint64) {
-	// defer close(c)
-	for i := uint64(0); i < size; i += 8 {
-		tmp := make([]byte, 8)
-		_, err := fin.Read(tmp)
-		if err != nil {
-			close(c)
-			return
-		}
-		c <- binary.LittleEndian.Uint64(tmp)
+func (s *ExternalSort) createReaderBuffer(filename string, size uint64) (fin *bufio.Reader, fileClose func() error, err error) {
+	inputFile, err := os.Open(filename)
+	if err != nil {
+		util.ErrorPrint(err, nil, "Open input file failed.")
+		return nil, nil, err
 	}
-	close(c)
+	return bufio.NewReaderSize(inputFile, int(size)), inputFile.Close, nil
 }
 
-func (s *ExternalSort) readToChan(fin *bufio.Reader,
-	size uint64) (c <-chan uint64) {
-	ch := make(chan uint64, 1024)
-	go s.readByteToUint64(fin, size, ch)
-	return ch
+func (s *ExternalSort) createWriterBuffer(filename string, size uint64) (fout *bufio.Writer, fileClose func() error, err error) {
+	s.tmpCountLock.Lock()
+	s.tmpCount++
+	tmpCount := s.tmpCount
+	s.tmpCountLock.Unlock()
+	outputFile, err := os.OpenFile(fmt.Sprintf("%s%s.%d%s",
+		TMP_FOLDER, filename, tmpCount, TMP_FILE_SUFFIX),
+		os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
+		util.ErrorPrint(err, nil, fmt.Sprintf("Open output file %d failed.", tmpCount))
+		return nil, nil, err
+	}
+	return bufio.NewWriterSize(outputFile, int(size)), outputFile.Close, nil
 }
 
-func (s *ExternalSort) internalSort(inChan <-chan uint64, fout *bufio.Writer) (err error) {
+func (s *ExternalSort) readOneUint64FromBuffer(fin *bufio.Reader) (x uint64, err error) {
+	tmp := make([]byte, 8)
+	n, err := fin.Read(tmp)
+	if n < 8 {
+		_, err = fin.Read(tmp[n:])
+	}
+	return binary.LittleEndian.Uint64(tmp), err
+}
+
+func (s *ExternalSort) readOneUint64FromFile(fin *os.File) (x uint64, err error) {
+	tmp := make([]byte, 8)
+	_, err = fin.Read(tmp)
+	return binary.LittleEndian.Uint64(tmp), err
+}
+
+func (s *ExternalSort) internalSort(fin *os.File, blockSize uint64) (err error) {
+	s.tmpCount++
+	outputFile, err := os.OpenFile(fmt.Sprintf("%s%s.%d%s",
+		TMP_FOLDER, s.filename, s.tmpCount, TMP_FILE_SUFFIX), os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
+		util.ErrorPrint(err, nil, "Open pre-writeen tmp file failed.")
+		return err
+	}
 	a := []uint64{}
-	for x := range inChan {
+	for i := 0; i < int(blockSize); i += 8 {
+		x, err := s.readOneUint64FromFile(fin)
+		if err != nil {
+			if err == io.EOF {
+				break
+			} else {
+				util.ErrorPrint(err, nil, "Reading input file failed.")
+				return err
+			}
+		}
 		a = append(a, x)
 	}
 	sort.Slice(a, func(i, j int) bool {
 		return a[i] < a[j]
 	})
 	for _, x := range a {
-		err := s.writeToBuffer(fout, x)
+		err := s.writeToFile(outputFile, x)
 		if err != nil {
 			return err
 		}
 	}
-	fmt.Printf("%d 0x%x 0x%x\n", len(a), a[0], a[len(a)-1])
+	s.blockSize = append(s.blockSize, blockSize)
+	fmt.Printf("Internal Sort: length = %d, from 0x%x to 0x%x\n", len(a), a[0], a[len(a)-1])
+	outputFile.Close()
+	return nil
+}
+
+func (s *ExternalSort) mergeSort(indexBegin int, k int, bufferSize uint64,
+	finishFlag chan<- int) (err error) {
+	outputBuffer, closeOutputFunc, err := s.createWriterBuffer(s.filename, bufferSize)
+	if err != nil {
+		return err
+	}
+	type inputType struct {
+		buf       *bufio.Reader
+		close     func() error
+		leng      uint64
+		completed bool
+	}
+	input := []inputType{}
+	num := []uint64{}
+	minIndex := -1
+	for i := 0; i < k; i++ {
+		buf, fun, err := s.createReaderBuffer(
+			fmt.Sprintf("%s%s.%d%s", TMP_FOLDER, s.filename, indexBegin+i, TMP_FILE_SUFFIX), bufferSize)
+		if err != nil {
+			return err
+		}
+
+		x, err := s.readOneUint64FromBuffer(buf)
+		if err != nil {
+			if err == io.EOF {
+				fun()
+			} else {
+				util.ErrorPrint(err, buf, "Read file failed for the first time in merge sort.")
+				return err
+			}
+		}
+		input = append(input, inputType{buf, fun, s.blockSize[indexBegin+i-1]/8 - 1, false})
+		num = append(num, x)
+		if minIndex == -1 || x < num[minIndex] {
+			minIndex = i
+		}
+	}
+
+	leng := 0
+	min := num[minIndex]
+	max := uint64(0)
+	for minIndex > -1 {
+		s.writeToBuffer(outputBuffer, num[minIndex])
+		max = num[minIndex]
+		leng++
+
+		if input[minIndex].leng > 0 {
+			num[minIndex], err = s.readOneUint64FromBuffer(input[minIndex].buf)
+			input[minIndex].leng--
+			if err != nil {
+				if err == io.EOF {
+					input[minIndex].close()
+					input[minIndex].leng = 0
+					input[minIndex].completed = true
+					minIndex = -1
+				} else {
+					util.ErrorPrint(err, input[minIndex], "Read file failed in merge sort.")
+					return err
+				}
+			} else {
+				if input[minIndex].leng == 0 {
+					input[minIndex].close()
+				}
+			}
+		} else {
+			input[minIndex].completed = true
+			minIndex = -1
+		}
+		for i := 0; i < k; i++ {
+			if !input[i].completed && (minIndex == -1 || num[i] < num[minIndex]) {
+				minIndex = i
+			}
+		}
+	}
+
+	s.blockSize = append(s.blockSize, uint64(leng*8))
+	fmt.Printf("Merge Sort: length = %d, index from %d to %d, num from 0x%x to 0x%x\n",
+		leng, indexBegin, indexBegin+k-1, min, max)
+	outputBuffer.Flush()
+	closeOutputFunc()
+	finishFlag <- 1
 	return nil
 }
 
@@ -93,60 +219,75 @@ func (s *ExternalSort) Sort(filename string, fileSize uint64,
 		return errors.New("memory limit should be divisible by 8")
 	}
 
-	inputFile, err := os.Open(DATA_FOLDER + filename + IN_FILE_SUFFIX)
+	s.filename = filename
+	os.Mkdir(TMP_FOLDER, 0644)
+
+	inputFile, err := os.Open(DATA_FOLDER + s.filename + IN_FILE_SUFFIX)
 	if err != nil {
-		util.ErrorPrint(err, nil, "Open input file failed.")
+		util.ErrorPrint(err, nil, "Read input file failed.")
 		return err
 	}
-
-	inputBuffer := bufio.NewReaderSize(inputFile, int(memLimit))
-	s.blockNum = int(math.Ceil(float64(fileSize) / float64(memLimit)))
+	s.blockNum = int((fileSize-1)/memLimit) + 1
+	s.tmpCount = 0
 	for i := 0; i < s.blockNum; i++ {
-		outputFile, err := os.OpenFile(fmt.Sprintf("%s%s.%d%s",
-			DATA_FOLDER, filename, i, INTERAL_SORT_TMP_FILE_SUFFIX),
-			os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
-		if err != nil {
-			util.ErrorPrint(err, nil, fmt.Sprintf("Open output file %d failed.", i))
-			return err
-		}
-		outputBuffer := bufio.NewWriter(outputFile)
-
-		inChan := s.readToChan(inputBuffer, memLimit)
-		err = s.internalSort(inChan, outputBuffer)
+		err = s.internalSort(inputFile, memLimit)
 		if err != nil {
 			util.ErrorPrint(err, nil, fmt.Sprintf("Internal sort %d failed", i))
 			return err
 		}
-		outputBuffer.Flush()
-		outputFile.Close()
 	}
 	inputFile.Close()
 
-	// fileIn, err := os.Open(DATA_FOLDER + filename + IN_FILE_SUFFIX)
-	// if err != nil {
-	// 	util.ErrorPrint(err, nil, "Open reading file failed.")
-	// 	return err
-	// }
-	// defer fileIn.Close()
-	// s.fin = bufio.NewReaderSize(fileIn, READ_FILE_BUFFER_SIZE)
+	nowIndex := 1
+	for s.blockNum > 1 {
+		var ioBufferSize uint64
+		thisIndex := s.tmpCount
+		var finishFlag chan int
+		t := 0
+		if s.blockNum%k == 1 {
+			ioBufferSize = uint64(math.Max(
+				float64(memLimit/uint64(s.blockNum-1+s.blockNum/k)), 1))
+			t = s.blockNum / k
+			finishFlag = make(chan int, t)
+		} else {
+			ioBufferSize = uint64(math.Max(
+				float64(memLimit/uint64(s.blockNum+(s.blockNum-1)/k+1)), 1))
+			t = (s.blockNum-1)/k + 1
+			finishFlag = make(chan int, t)
+		}
+		for nowIndex < thisIndex {
+			if thisIndex-nowIndex+1 < k {
+				go s.mergeSort(nowIndex, thisIndex-nowIndex+1, ioBufferSize, finishFlag)
+				nowIndex = thisIndex + 1
+			} else {
+				go s.mergeSort(nowIndex, k, ioBufferSize, finishFlag)
+				nowIndex += k
+			}
+		}
 
-	// fileOut, err := os.OpenFile(DATA_FOLDER+filename+OUT_FILE_SUFFIX,
-	// 	os.O_WRONLY|os.O_CREATE, 0644)
-	// if err != nil {
-	// 	util.ErrorPrint(err, nil, "Open writing file failed.")
-	// 	return err
-	// }
-	// defer fileOut.Close()
-	// s.fout = bufio.NewWriterSize(fileOut, WRITE_FILE_BUFFER_SIZE)
-	// defer s.fout.Flush()
+		flagCount := 0
+		for x := range finishFlag {
+			flagCount += x
+			if flagCount == t {
+				break
+			}
+		}
+		close(finishFlag)
+		s.blockNum = s.tmpCount - nowIndex + 1
+	}
 
-	// p := make([]byte, READ_FILE_BUFFER_SIZE)
-	// for _, err := s.fin.Read(p); err == nil; _, err = s.fin.Read(p) {
-	// 	_, err = s.fout.Write(p)
-	// 	if err != nil {
-	// 		return err
-	// 	}
-	// }
+	err = os.Rename(fmt.Sprintf("%s%s.%d%s", TMP_FOLDER, s.filename,
+		s.tmpCount, TMP_FILE_SUFFIX), DATA_FOLDER+s.filename+OUT_FILE_SUFFIX)
+	if err != nil {
+		util.ErrorPrint(err, nil, fmt.Sprintf("Failed to rename output file %s%s.%d%s", TMP_FOLDER,
+			s.filename, s.tmpCount, TMP_FILE_SUFFIX))
+		return nil
+	}
+
+	err = os.RemoveAll(TMP_FOLDER)
+	if err != nil {
+		util.ErrorPrint(err, nil, "Error occur while remove tmp file.")
+	}
 
 	return nil
 }
--- externalSort/externalsort_test.go
+++ externalSort/externalsort_test.go
@@ -2,16 +2,25 @@ package externalsort_test
 
 import (
 	"encoding/binary"
+	"fmt"
 	"math/rand"
 	"os"
+	"sort"
 	"testing"
 
 	. "externalsort"
 )
 
+func sortA(a *[]uint64, flag chan<- bool) {
+	sort.Slice(*a, func(i, j int) bool {
+		return (*a)[i] < (*a)[j]
+	})
+	flag <- true
+}
+
 func TestExternalSort(t *testing.T) {
 	const FILENAME = "test"
-	const FILESIZE = 8 * (1 << 20) // 1 << (3 + 20)
+	const FILESIZE = 8 * (1 << 22)
 	const MEMORY_LIMIT = 1 << 15
 	file, err := os.OpenFile(DATA_FOLDER+FILENAME+IN_FILE_SUFFIX,
 		os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
@@ -19,19 +28,43 @@ func TestExternalSort(t *testing.T) {
 		t.Error(err, "Open test writing file failed.")
 		return
 	}
-	defer file.Close()
 
 	const n = FILESIZE / 8
 	tmp := make([]byte, 8)
+	a := []uint64{}
 	for i := 0; i < n; i++ {
-		binary.LittleEndian.PutUint64(tmp, rand.Uint64())
+		x := rand.Uint64()
+		a = append(a, x)
+		binary.LittleEndian.PutUint64(tmp, x)
 		file.Write(tmp)
 	}
+	flag := make(chan bool)
+	go sortA(&a, flag)
+	file.Close()
 
 	s := ExternalSort{}
 	err = s.Sort(FILENAME, FILESIZE, MEMORY_LIMIT, 2)
 	if err != nil {
 		t.Error(err, "Sort failed.")
 		return
+	}
+	<-flag
+	file, err = os.OpenFile(DATA_FOLDER+FILENAME+OUT_FILE_SUFFIX,
+		os.O_RDONLY, 0644)
+	if err != nil {
+		t.Error(err, "Open answer file failed.")
+		return
+	}
+	for i := 0; i < n; i++ {
+		_, err := file.Read(tmp)
+		if err != nil {
+			t.Error(err, "Read answer file failed.")
+			return
+		}
+		x := binary.LittleEndian.Uint64(tmp)
+		if x != a[i] {
+			t.Log(fmt.Sprintf("Wrong answer at %d number, read 0x%x, expected 0x%x.", i, x, a[i]))
+			t.FailNow()
+		}
 	}
 }
